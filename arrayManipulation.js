50c498ccf0ebfaeff1863194924280239c6f5ee5
Eqnerret <terrencekedzy@gmail.com>
2024-05-19 17:34:19 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -10,7 +10,32 @@ function processArray(arr) {
   });
 }
 
+
+// arrayManipulation.js
+
+function processArray(arr) {
+  return arr.map(num => {
+    if (num % 2 === 0) {
+      return num * num;
+    } else {
+      return num * 3;
+    }
+  });
+}
+
+function formatArrayStrings(stringArr, numArr) {
+  return stringArr.map((str, index) => {
+    if (numArr[index] % 2 === 0) {
+      return str.toUpperCase();
+    } else {
+      return str.toLowerCase();
+    }
+  });
+}
+
 // Example usage:
+let strings = ['My', 'Name', 'Is', 'Terrence', 'Adams'];
 let numbers = [1, 2, 3, 4, 5];
 let processedNumbers = processArray(numbers);
-console.log(processedNumbers); // Output: [3, 16, 9, 16, 15]
+let formattedStrings = formatArrayStrings(strings, numbers); // Using original numbers array
+console.log(formattedStrings); // Output: ["MY", "NAME", "IS", "TERRENCE", "adams"]
