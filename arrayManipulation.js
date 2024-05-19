1f89883279a9dd5e339edab2611750d12bb59282
Eqnerret <terrencekedzy@gmail.com>
2024-05-19 17:15:06 +0000

    Task 1

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,28 +1,16 @@
 // arrayManipulation.js
 
 function processArray(arr) {
-    return arr.map(num => {
-      if (num % 2 === 0) {
-        return num * num;
-      } else {
-        return num * 3;
-      }
-    });
-  }
-  
-  function formatArrayStrings(stringArr, numArr) {
-    return stringArr.map((str, index) => {
-      if (numArr[index] % 2 === 0) {
-        return str.toUpperCase();
-      } else {
-        return str.toLowerCase();
-      }
-    });
-  }
-  
-  // Example usage:
-  let strings = ['Hello', 'World', 'JavaScript', 'is', 'fun'];
-  let numbers = [1, 2, 3, 4, 5];
-  let processedNumbers = processArray(numbers);
-  let formattedStrings = formatArrayStrings(strings, processedNumbers);
-  console.log(formattedStrings); // Output: ["hello", "WORLD", "javascript", "IS", "fun"
+  return arr.map(num => {
+    if (num % 2 === 0) {
+      return num * num;
+    } else {
+      return num * 3;
+    }
+  });
+}
+
+// Example usage:
+let numbers = [1, 2, 3, 4, 5];
+let processedNumbers = processArray(numbers);
+console.log(processedNumbers); // Output: [3, 16, 9, 16, 15]
